1eb8cac4b87b001de909260a7291fc59533ecaff
Parabeac-Nest <admin@parabeac.com>
2022-11-16 07:38:46 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ android/app/src/main/kotlin/com/example/nilami/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nilami
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/nilami_colors.g.dart
@@ -0,0 +1,18 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class NilamiColors {
+
+static const Color dark = Color(0xff14181b);
+
+static const Color gray6 = Color(0xfff2f2f2);
+
+static const Color orange = Color(0xfffc9905);
+
+static const Color gray4 = Color(0xffbdbdbd);
+}
